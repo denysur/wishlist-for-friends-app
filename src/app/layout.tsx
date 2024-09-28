e011de5a58ed6e7bfe13cb475adf711d13a69e31
Denys Urla <urladenys.dev.com>
2024-09-28 18:48:19 +0300

    feat: Initialize Next JS project

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Wishlist For Friends",
+  description:
+    "This application is a tool that allows users to create and manage lists of desired items or services. These applications are popular among shoppers, gift enthusiasts, and people who want to plan their purchases.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+export default function HomePage() {
+  return (
+    <div>
+      <main>
+        <h5>TBD</h5>
+        <h1>Wishlist For Friends</h1>
+        <p>
+          This application is a tool that allows users to create and manage
+          lists of desired items or services. These applications are popular
+          among shoppers, gift enthusiasts, and people who want to plan their
+          purchases.
+        </p>
+      </main>
+      <footer></footer>
+    </div>
+  );
+}
